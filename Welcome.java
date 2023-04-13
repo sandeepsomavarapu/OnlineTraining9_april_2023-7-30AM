810c87a0d060b3b335629917204a8ae8175e90c3
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-04-13 08:51:54 +0530

    Day-2 13-04-2023

---
+++ Welcome.java
@@ -0,0 +1,29 @@
+
+class Welcome
+{
+	
+public static void main(String args[])//jdk-->jre-->JVM
+{
+	
+	System.out.println("welcome to java");
+	
+	
+}	
+	
+	
+}
+
+//machine language --->0's 1's / binary /byte 
+
+//translation-->translator
+
+//compailation-->compiler   user understandable lang-->machine understandable lang
+
+//source code -->byte code 
+
+// .java -->.class-(JVM)
+
+
+//javac filename.java
+
+//java classname   jvm -->main
